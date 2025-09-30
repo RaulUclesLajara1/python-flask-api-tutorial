68f303c2c11b92eb11dbeed611a8ad75786bafca
Raul Ucles <rucleslajara@gmail.com>
2025-09-30 18:36:15 +0000

    Learnpack realizado

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-todos = [ { "label": "My first task", "done": False } ]
+todos = [{"label":"Sacar al perro","done":False}]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
@@ -20,7 +20,5 @@ def delete_todo(position):
     todos.pop(position)
     return jsonify(todos)
 
-
-# These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
